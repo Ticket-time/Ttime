48d31d1d6bf3be8353062825d8f3e480638a8940
dhlrwu <yso7414@sookmyung.ac.kr>
2023-06-16 08:06:31 +0900

    [checkPayable return code 속성 추가]
    111, 222, 333, 444

--- backend/controllers/check.js
+++ backend/controllers/check.js
@@ -13,9 +13,9 @@ exports.checkPayable = async (req, res) => {
   var showid = req.body.showid;
   var sysdate = new Date();
 
-  const sql1 = "SELECT isWin FROM apply WHERE userid=? and showid=?;";
+  const sql1 = "SELECT isWin, payment FROM apply WHERE userid=? and showid=?;";
   var params = [userid, showid];
-  
+
   const sql1s = mysql.format(sql1, params);
 
   const sql2 = "SELECT paystart, payend FROM shows WHERE showid=?;";
@@ -23,7 +23,7 @@ exports.checkPayable = async (req, res) => {
 
   db.query(sql1s + sql2s, async (err, results) => {
     let sql1s_result = JSON.parse(JSON.stringify(results[0]));
-   
+
     let sql2s_result = JSON.parse(JSON.stringify(results[1]));
     sql1s_result = sql1s_result[0];
     sql2s_result = sql2s_result[0];
@@ -35,7 +35,7 @@ exports.checkPayable = async (req, res) => {
     // 당첨 여부 확인
     if (sql1s_result.isWin == 0) {
       console.log("추첨 결과: X");
-      return res.send({ success: false, message: "당첨X" });
+      return res.send({ success: false, message: "당첨X", code: 111 });
     }
     // 기간 확인 paystart ~ payend 사이일 것
     let _paystart = new Date(sql2s_result.paystart);
@@ -45,15 +45,30 @@ exports.checkPayable = async (req, res) => {
     console.log(_payend);
     if (_paystart <= sysdate && _payend > sysdate) {
       console.log("결제 기간 내 입니다.");
-      return res.send({
-        success: true,
-        message: "결제 가능",
-      });
+      let code;
+      if (sql1s_result.payment == true) {
+        //이미 결제 완료
+        code = 222;
+        return res.send({
+          success: false,
+          message: "결제가 이미 완료됨",
+          code: 222,
+        });
+      } else {
+        // 결제 안 된 상태 - 이 상태만 결제가 가능
+        code = 333;
+        return res.send({
+          success: true,
+          message: "결제 가능",
+          code: 333,
+        });
+      }
     } else {
-      console.log("결제 기간이 지났습니다.");
+      console.log("결제 기간이 지났습니다. 혹은 아직입니다.");
       return res.send({
         success: false,
         message: "결제 불가",
+        code: 444,
       });
     }
   });
