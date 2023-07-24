224eb5d01fc2233c9716140bc53bda9e0ffc2758
dhlrwu <yso7414@sookmyung.ac.kr>
2023-07-24 17:50:09 +0900

    거래 testcode 작성

--- contracts/Ticketing.sol
+++ contracts/Ticketing.sol
@@ -69,7 +69,6 @@ contract Ticketing {
                 j++;
             }
         }
-
         return resellTicketList;
     }
 
--- test/testTicket_ys.js
+++ test/testTicket_ys.js
@@ -1,82 +1,68 @@
-const Ticket = artifacts.require("Ticket");
+const Ticketing = artifacts.require("Ticketing");
 // const { isTopic } = require("web3-utils");
 
-// 확인해야 하는 것
-// 구매자 - 티켓 셀러 간 송금 제대로 되는지
-contract("test", (accounts) => {
-  it.only("응모 성공", async () => {
-    const ticketInstance = await Ticket.deployed({
-      from: accounts[0],
-      value: 37000000000000000,
+contract("Ticketing", (accounts) => {
+  it("공연1 거래 내역 불러오기", async () => {
+    const instance = await Ticketing.deployed();
+    // 1번 공연 - 3개 티켓 발행
+    await instance.issueTicket(1, accounts[3], {
+      value: 4000000000000000, // 너무 큰 수
     });
-    await ticketInstance.book({ from: accounts[0] });
-    await ticketInstance.book({ from: accounts[1] });
-    await ticketInstance.book({ from: accounts[2] });
-    await ticketInstance.book({ from: accounts[3] });
-    await ticketInstance.book({ from: accounts[4] });
-    await ticketInstance.book({ from: accounts[5] });
-    await ticketInstance.book({ from: accounts[6] });
-    await ticketInstance.book({ from: accounts[7] });
-    await ticketInstance.book({ from: accounts[8] });
-    applicantList = await ticketInstance.get();
-    assert.equal(applicantList.length, 9, "응모자 수 불일치");
+    await instance.issueTicket(1, accounts[2], {
+      value: 4000000000000000,
+    });
+    await instance.issueTicket(1, accounts[1], {
+      value: 4000000000000000,
+    });
+    await instance.resellTicket(1, 1, {
+      from: accounts[3],
+    });
+    await instance.resellTicket(1, 2, {
+      from: accounts[2],
+    });
+    const response = await instance.getResellTicket(1);
+    const num = response.length;
+    assert.equal(num, 2, "양도 티켓 개수가 틀립니다.");
   });
-  it("random() 성공", async () => {
-    const ticketInstance = await Ticket.deployed({
-      from: accounts[0],
-      value: 37000000000000000,
+
+  it("공연4 거래 내역 불러오기", async () => {
+    const instance = await Ticketing.deployed();
+    await instance.issueTicket(4, accounts[9], {
+      value: 1000000000000000,
     });
-    await ticketInstance.book({ from: accounts[0] });
-    await ticketInstance.book({ from: accounts[1] });
-    await ticketInstance.book({ from: accounts[2] });
-    await ticketInstance.book({ from: accounts[3] });
-    await ticketInstance.book({ from: accounts[4] });
-    await ticketInstance.book({ from: accounts[5] });
-    await ticketInstance.book({ from: accounts[6] });
-    await ticketInstance.book({ from: accounts[7] });
-    await ticketInstance.book({ from: accounts[8] });
-
-    await ticketInstance.random();
-
-    const seatList = await ticketInstance.getSeats();
-
-    var arr = new Array(9);
-    arr.fill(0);
-    for (var i = 0; i < 3; i++) {
-      num = seatList[i];
-      arr[num]++;
-    }
-    var randomCheck = true;
-    for (var i = 0; i < 9; i++) {
-      if (arr[i] > 1) {
-        randomCheck = false;
-      }
-    }
-    assert.equal(randomCheck, true, "random 함수 중복 할당");
+    await instance.issueTicket(4, accounts[8], {
+      value: 1000000000000000,
+    });
+    await instance.issueTicket(4, accounts[7], {
+      value: 1000000000000000,
+    });
+    await instance.resellTicket(4, 3, {
+      from: accounts[7],
+    });
+    await instance.resellTicket(4, 2, {
+      from: accounts[8],
+    });
+    await instance.resellTicket(4, 1, {
+      from: accounts[9],
+    });
+    const response = await instance.getResellTicket(4);
+    assert.equal(response.length, 3, "양도 티켓 개수가 틀립니다.");
   });
-  it("buyTicket() 성공", async () => {
-    const ticketInstance = await Ticket.deployed({
-      from: accounts[0],
-      value: 37000000000000000,
+
+  it("공연3 거래 내역 불러오기", async () => {
+    const instance = await Ticketing.deployed();
+    const response = await instance.getResellTicket(3);
+    assert.equal(response, false, "거래 내역이 존재합니다.");
+  });
+
+  it("resellTicketList 세부 내역 확인", async () => {
+    const instance = await Ticketing.deployed();
+    const f = await instance.resellTicket(1, 3, {
+      from: accounts[1],
     });
+    const list = await instance.getResellTicket(1);
 
-    const buyer = accounts[0];
-    const seller = accounts[9];
-    const value = 37000000000000000;
-
-    const sellerStartingBalance = await web3.eth.getBalance(seller);
-
-    // [0] 계정이 컨트랙트에 송금
-    // 컨트랙트에서 [9] 계정으로 다시 송금
-    // [0] -> contract -> [9]
-    await ticketInstance.buyTicket(seller, { from: buyer });
-
-    const sellerEndingBalance = await web3.eth.getBalance(seller);
-
-    assert.equal(
-      sellerEndingBalance,
-      sellerStartingBalance + value,
-      "Amount wasn't correctly sent to the receiver"
-    );
+    const owner = await list[2].owner;
+    assert.equal(owner, accounts[1], "티켓 주인 계정이 다릅니다.");
   });
 });
