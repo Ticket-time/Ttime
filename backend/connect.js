3e15b5a349c89c74e23aa4022ec836223c8c015b
dhlrwu <yso7414@sookmyung.ac.kr>
2023-09-08 17:03:54 +0900

    connect.js 리팩토링

--- backend/connect.js
+++ backend/connect.js
@@ -5,196 +5,155 @@ const ticketing_artifact = require("../build/contracts/Ticketing.json");
 var Ticketing = contract(ticketing_artifact);
 const db = require("./util/database");
 const fs = require("fs");
+const Show = require("./models/show");
+
 module.exports = {
-  createShow: function (showOwner, ticketPriceEth, callback) {
-    var self = this;
-    const ticketPriceWei = web3.toWei(ticketPriceEth, "ether");
-    console.log(ticketPriceWei);
-    Ticketing.setProvider(self.web3.currentProvider);
-    var ticketing;
-    Ticketing.deployed()
-      .then(function (instance) {
-        ticketing = instance;
-        return ticketing.createShow(ticketPriceWei, { from: showOwner });
-      })
-      .then(function (value) {
-        console.log(value.receipt.status);
-        callback(value.receipt.status);
-      })
-      .catch(function (e) {
-        console.log(e);
-        callback("ERROR 404");
+  createShow: async function (showOwner, ticketPriceEth, callback) {
+    try {
+      const self = this;
+      const ticketPriceWei = web3.toWei(ticketPriceEth, "ether");
+      console.log(ticketPriceWei);
+      await Ticketing.setProvider(self.web3.currentProvider);
+      const ticketing = await Ticketing.deployed();
+      const value = await ticketing.createShow(ticketPriceWei, {
+        from: showOwner,
       });
+      callback(value.receipt.status);
+    } catch (e) {
+      console.log(e);
+      callback("ERROR 404");
+    }
   },
-  issueTicket: function (showId, ticketOwner, callback) {
-    var self = this;
-    Ticketing.setProvider(self.web3.currentProvider);
-    var ticketing;
-
-    Ticketing.deployed()
-      .then(function (instance) {
-        ticketing = instance;
-        return ticketing.getTicketPrice(showId);
-      })
-      .then(function (price) {
-        console.log(`price: ${price}`);
-        return ticketing.issueTicket(showId, ticketOwner, {
-          from: ticketOwner,
-          value: price,
-        });
-      })
-      .then(async function (value) {
-        // update payment in apply table
-        const userId = await db.query("select id from user where wallet = ?", [
-          ticketOwner,
-        ]);
-        const rows = await db.query(
-          "update apply set payment=1 where showid = ? and userid = ?",
-          [showId, userId[0][0].id]
-        );
-        if (rows.length > 0) {
-          console.log("apply table 업데이트 완료");
-        }
-
-        console.log(`check receipt`);
-        let tx = value.tx;
-        // log 확인
-        for (var i = 0; i < value.logs.length; i++) {
-          var log = value.logs[i];
-          if (log.event == "ISSUE_TICKET") {
-            // We found the event!
-            let param1 = log.args._showId;
-            let param2 = log.args._ticketId;
-            if (showId == param1) {
-              callback({
-                tx: tx,
-                message: `showId: ${param1}, ticketId: ${param2}`,
-              });
-            }
-            break;
-          }
-        }
-      })
-      .catch(function (e) {
-        console.log(e);
-        callback("발급 오류");
+  issueTicket: async function (showId, ticketOwner, callback) {
+    try {
+      const self = this;
+      await Ticketing.setProvider(self.web3.currentProvider);
+      const ticketing = await Ticketing.deployed();
+      const price = await ticketing.getTicketPrice(showId);
+      const result = await ticketing.issueTicket(showId, ticketOwner, {
+        from: ticketOwner,
+        value: price,
       });
+      console.log(`price: ${price}`);
+      const userId = await db.query("select id from user where wallet = ?", [
+        ticketOwner,
+      ]);
+      const rows = await db.query(
+        "update apply set payment=1 where showid = ? and userid = ?",
+        [showId, userId[0][0].id]
+      );
+      if (rows.length > 0) {
+        console.log("apply table 업데이트 완료");
+      }
+      console.log(`check receipt`);
+      let tx = result.tx;
+
+      // log 확인
+      for (let i = 0; i < result.logs.length; i++) {
+        let log = result.logs[i];
+        if (log.event == "ISSUE_TICKET") {
+          // We found the event!
+          let param1 = log.args._showId;
+          let param2 = log.args._ticketId;
+          if (showId == param1) {
+            callback({
+              tx: tx,
+              message: `showId: ${param1}, ticketId: ${param2}`,
+            });
+          }
+          break;
+        }
+      }
+    } catch (e) {
+      console.log(e);
+      callback("발급 오류");
+    }
   },
   getMyTicket: async function (userAddr, callback) {
-    var self = this;
-    Ticketing.setProvider(self.web3.currentProvider);
-    var ticketing;
-
-    Ticketing.deployed()
-      .then(function (instance) {
-        ticketing = instance;
-
-        return instance.getMyTicket(userAddr);
-      })
-      .then(async function (result) {
-        // result는 object 배열이다
-        let array = [];
-        console.log(result.length);
-        if (result.length === 0) {
-          return callback({
-            success: true,
-            message: "보유 티켓 없음",
-            data: [],
-          });
-        }
-
-        // json 으로 변환 - 이름 , 날짜, 시간, 이미지 세로긴거, 일시, 장소, 좌석
-        for (var i = 0; i < result.length; i++) {
-          try {
-            const rows = await db.query(
-              "select * from shows where showid = ?",
-              [result[i].showId]
-            );
-            console.log("hi");
-            rows[0][0].ticketId = result[i].ticketId;
-            let showid = rows[0][0].showid;
-            let imgFile = fs.readFileSync(`./image/${showid}.jpg`);
-            let encode = Buffer.from(imgFile).toString("base64");
-            rows[0][0].imgEncode = encode;
-            array.push(rows[0][0]);
-            console.log(array[0]);
-          } catch (err) {
-            console.log(err);
-            throw err;
-          }
-        }
-        console.log(array); // for문 끝
-        callback({
-          success: true,
-          message: "my ticket 가져오기 성공",
-          data: array,
-        });
+    const self = this;
+    await Ticketing.setProvider(self.web3.currentProvider);
+    const ticketing = await Ticketing.deployed();
+    const result = await ticketing.getMyTicket(userAddr);
+    let array = [];
+    if (result.length === 0) {
+      return callback({
+        success: true,
+        message: "보유 티켓 없음",
+        data: [],
       });
+    }
+    for (var i = 0; i < result.length; i++) {
+      try {
+        // const rows = await db.query("select * from shows where showid = ?", [
+        //   result[i].showId,
+        // ]);
+        const rows = await Show.findById(result[i].showId);
+        rows[0][0].ticketId = result[i].ticketId;
+        let showid = rows[0][0].showid;
+        let imgFile = fs.readFileSync(`./image/${showid}.jpg`);
+        let encode = Buffer.from(imgFile).toString("base64");
+        rows[0][0].imgEncode = encode;
+        array.push(rows[0][0]);
+        console.log(array[0]);
+      } catch (err) {
+        console.log(err);
+        throw err;
+      }
+    }
+    console.log(array); // for문 끝
+    callback({
+      success: true,
+      message: "my ticket 가져오기 성공",
+      data: array,
+    });
   },
   getResellTicket: async function (showId, callback) {
-    var self = this;
-    Ticketing.setProvider(self.web3.currentProvider);
-    var ticketing;
-
-    Ticketing.deployed()
-      .then(function (instance) {
-        ticketing = instance;
-
-        return instance.getResellTicket(showId);
-      })
-      .then(async function (result) {
-        let array = [];
-        console.log(result.length);
-        if (result.length === 0) {
-          return callback({
-            success: true,
-            message: "해당 공연 tx 없음",
-            data: [],
-          });
-        }
-        for (var i = 0; i < result.length; i++) {
-          try {
-            const rows = await db.query(
-              "select * from shows where showid = ?",
-              [result[i].showId]
-            );
-            rows[0][0].ticketId = result[i].ticketId;
-            let showid = rows[0][0].showid;
-            let imgFile = fs.readFileSync(`./image/${showid}.jpg`);
-            let encode = Buffer.from(imgFile).toString("base64");
-            rows[0][0].imgEncode = encode;
-            array.push(rows[0][0]);
-            console.log(array[0]);
-          } catch (err) {
-            console.log(err);
-            throw err;
-          }
-        }
-        callback({
-          success: true,
-          message: "tx 가져오기 성공",
-          data: array,
-        });
+    const self = this;
+    await Ticketing.setProvider(self.web3.currentProvider);
+    const ticketing = await Ticketing.deployed();
+    const result = await ticketing.getResellTicket(showId);
+    let array = [];
+    if (result.length === 0) {
+      return callback({
+        success: true,
+        message: "해당 공연 tx 없음",
+        data: [],
       });
+    }
+    for (var i = 0; i < result.length; i++) {
+      try {
+        const rows = await db.query("select * from shows where showid = ?", [
+          result[i].showId,
+        ]);
+        rows[0][0].ticketId = result[i].ticketId;
+        let showid = rows[0][0].showid;
+        let imgFile = fs.readFileSync(`./image/${showid}.jpg`);
+        let encode = Buffer.from(imgFile).toString("base64");
+        rows[0][0].imgEncode = encode;
+        array.push(rows[0][0]);
+        console.log(array[0]);
+      } catch (err) {
+        console.log(err);
+        throw err;
+      }
+    }
+    callback({
+      success: true,
+      message: "tx 가져오기 성공",
+      data: array,
+    });
   },
 
   resellTicket: async function (showId, ticketId, userAddr, callback) {
-    var self = this;
-    Ticketing.setProvider(self.web3.currentProvider);
-    var ticketing;
-    Ticketing.deployed()
-      .then(function (instance) {
-        ticketing = instance;
-
-        return instance.resellTicket(showId, ticketId, {
-          from: userAddr,
-        });
-      })
-      .then(
-        callback({
-          success: true,
-          message: "양도 탭에 티켓 추가 완료",
-        })
-      );
+    const self = this;
+    await Ticketing.setProvider(self.web3.currentProvider);
+    const ticketing = await Ticketing.deployed();
+    try {
+      await ticketing.resellTicket(showId, ticketId, { from: userAddr });
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+    callback({ success: true, message: "양도 탭에 티켓 추가 완료" });
   },
 };
--- test/testTicket_ys.js
+++ test/testTicket_ys.js
@@ -1,18 +1,18 @@
 const Ticketing = artifacts.require("Ticketing");
-// const { isTopic } = require("web3-utils");
 
 contract("Ticketing", (accounts) => {
-  it("공연1 거래 내역 불러오기", async () => {
+  it("tx 내역 불러오기 - 공연 1", async () => {
     const instance = await Ticketing.deployed();
-    // 1번 공연 - 3개 티켓 발행
+    const price = 10 ** 16 * 4;
+    // 1번 공연 - 3개 티켓 발행, 2개 양도
     await instance.issueTicket(1, accounts[3], {
-      value: 4000000000000000, // 너무 큰 수
+      value: price,
     });
     await instance.issueTicket(1, accounts[2], {
-      value: 4000000000000000,
+      value: price,
     });
     await instance.issueTicket(1, accounts[1], {
-      value: 4000000000000000,
+      value: price,
     });
     await instance.resellTicket(1, 1, {
       from: accounts[3],
@@ -20,21 +20,24 @@ contract("Ticketing", (accounts) => {
     await instance.resellTicket(1, 2, {
       from: accounts[2],
     });
+
     const response = await instance.getResellTicket(1);
-    const num = response.length;
-    assert.equal(num, 2, "양도 티켓 개수가 틀립니다.");
+
+    assert.equal(response.length, 2, "양도 티켓 개수가 틀립니다.");
   });
 
-  it("공연4 거래 내역 불러오기", async () => {
+  it("tx 내역 불러오기 - 공연 4", async () => {
     const instance = await Ticketing.deployed();
+    const price = 10 ** 16 * 5;
+    // 4번 공연 티켓 3개 발행, 3개 양도
     await instance.issueTicket(4, accounts[9], {
-      value: 1000000000000000,
+      value: price,
     });
     await instance.issueTicket(4, accounts[8], {
-      value: 1000000000000000,
+      value: price,
     });
     await instance.issueTicket(4, accounts[7], {
-      value: 1000000000000000,
+      value: price,
     });
     await instance.resellTicket(4, 3, {
       from: accounts[7],
@@ -45,11 +48,13 @@ contract("Ticketing", (accounts) => {
     await instance.resellTicket(4, 1, {
       from: accounts[9],
     });
+
     const response = await instance.getResellTicket(4);
+
     assert.equal(response.length, 3, "양도 티켓 개수가 틀립니다.");
   });
 
-  it("공연3 거래 내역 불러오기", async () => {
+  it("tx 내역 불러오기 - 공연 3", async () => {
     const instance = await Ticketing.deployed();
     const response = await instance.getResellTicket(3);
     assert.equal(response, false, "거래 내역이 존재합니다.");
@@ -57,12 +62,33 @@ contract("Ticketing", (accounts) => {
 
   it("resellTicketList 세부 내역 확인", async () => {
     const instance = await Ticketing.deployed();
-    const f = await instance.resellTicket(1, 3, {
+    await instance.resellTicket(1, 3, {
       from: accounts[1],
     });
     const list = await instance.getResellTicket(1);
 
     const owner = await list[2].owner;
     assert.equal(owner, accounts[1], "티켓 주인 계정이 다릅니다.");
+  });
+
+  it("createShow 공연 생성", async () => {
+    const instance = await Ticketing.deployed();
+    await instance.createShow(
+      2000000000000000,
+      "0x43432190d425F0BCeE18F3C0E011D334A8a5f893"
+    );
+    const showIndex = await instance.showIndex();
+    assert.equal(showIndex, 7, "show 생성이 이루어지지 않았습니다.");
+  });
+
+  it("getMyTicket", async () => {
+    const instance = await Ticketing.deployed();
+    const price = 10 ** 16 * 4;
+    // 2번 공연 티켓 발행, 1개 양도
+    await instance.issueTicket(2, accounts[4], {
+      value: price,
+    });
+    let ticketArray = await instance.getMyTicket(accounts[4]);
+    assert.equal(ticketArray.length, 1, "티켓 개수가 맞지 않습니다.");
   });
 });
